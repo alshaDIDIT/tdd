f2fa5c631766bd2d68a7c2a32f88c3c956d95e9d
Youssef Al Shadidi <youssef.as@gmail.com>
2022-09-22 11:26:55 +0200

    Second and third, verify jwt token test done

--- src/test/java/se/alshadidi/LoginTest.java
+++ src/test/java/se/alshadidi/LoginTest.java
@@ -36,9 +36,9 @@ public class LoginTest {
     @CsvSource(value = {"anna, losen, YW5uYQ==", "berit, 123456, YmVyaXQ=", "kalle, password, a2FsbGU="})
     public void login_with_mock(String username, String password, String expected) {
         when(userRepository.findAll()).thenReturn(List.of(
-                new AppUser(1,"anna", "losen", "ADMIN"),
-                new AppUser(2,"berit", "123456", "TEACHER"),
-                new AppUser(3,"kalle", "password", "STUDENT")
+                        new AppUser(1, "anna", "losen", "ADMIN"),
+                        new AppUser(2, "berit", "123456", "TEACHER"),
+                        new AppUser(3, "kalle", "password", "STUDENT")
                 )
         );
 
@@ -50,7 +50,7 @@ public class LoginTest {
     @Test
     public void login_unhappy_path() {
         when(userRepository.findAll()).thenReturn(List.of(
-                        new AppUser(1,"anna", "losen", "ADMIN")
+                        new AppUser(1, "anna", "losen", "ADMIN")
                 )
         );
         String username = "anna";
@@ -64,7 +64,7 @@ public class LoginTest {
     public void login_with_token(String token, boolean expected) {
         // given
         when(userRepository.findAll()).thenReturn(List.of(
-                        new AppUser(1,"anna", "losen", "ADMIN")
+                        new AppUser(1, "anna", "losen", "ADMIN")
                 )
         );
 
@@ -80,12 +80,12 @@ public class LoginTest {
             "1, eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhbm5hIiwiUm9sZSI6IkFETUlOIn0.KyBpzBcEOBQdhJlxD0aQIW8pVy-jwiNTBIdeTzyb1tPRQVi1HkGmu53xlRDYn0Dj",
             "2, eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJiZXJpdCIsIlJvbGUiOiJURUFDSEVSIn0.MrPaAaygKMUvPdOdgdU4Khy9BVHrhCZ-f5n7yevF2_bWxjXfFekXvHnS7fmE30Wg",
             "3, eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJrYWxsZSIsIlJvbGUiOiJTVFVERU5UIn0.wubfKhTSs_uSPIqkM1xkuuSl0J4zr1nj3U8fMMyP1VKOADXjBVjVqfR5oz1rEJrJ"})
-    public void login_with_jwt_token(int id, String expected){
+    public void login_with_jwt_token(int id, String expected) {
         // given
         when(userRepository.findAll()).thenReturn(List.of(
-                        new AppUser(1,"anna", "losen", "ADMIN"),
-                        new AppUser(2,"berit", "123456", "TEACHER"),
-                        new AppUser(3,"kalle", "password", "STUDENT")
+                        new AppUser(1, "anna", "losen", "ADMIN"),
+                        new AppUser(2, "berit", "123456", "TEACHER"),
+                        new AppUser(3, "kalle", "password", "STUDENT")
                 )
         );
 
@@ -98,21 +98,55 @@ public class LoginTest {
 
     @Test
     public void verify_token_test() {
-        // given
         when(userRepository.findAll()).thenReturn(List.of(
-                        new AppUser(1,"anna", "losen", "ADMIN")
+                        new AppUser(1, "anna", "losen", "ADMIN")
                 )
         );
         String jwtToken = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhbm5hIiwiUm9sZSI6IkFETUlOIn0.KyBpzBcEOBQdhJlxD0aQIW8pVy-jwiNTBIdeTzyb1tPRQVi1HkGmu53xlRDYn0Dj";
 
-        // when
         List<Map<String, String>> result = login.verifyToken(jwtToken);
 
         List<Map<String, String>> expected = new ArrayList<>();
         expected.add(1, Map.of("GRADING", "READ"));
         expected.add(2, Map.of("GRADING", "WRITE"));
-        expected.add(3, Map.of("COURSE_FEEDBACK", "WRITE"));
+        expected.add(3, Map.of("COURSE_FEEDBACK", "READ"));
         expected.add(4, Map.of("COURSE_FEEDBACK", "WRITE"));
+
+        assertIterableEquals(expected, result);
+    }
+
+    @Test
+    public void verify_token_test_two() {
+        when(userRepository.findAll()).thenReturn(List.of(
+                        new AppUser(2, "berit", "123456", "TEACHER")
+                )
+        );
+        String jwtToken = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJiZXJpdCIsIlJvbGUiOiJURUFDSEVSIn0.MrPaAaygKMUvPdOdgdU4Khy9BVHrhCZ-f5n7yevF2_bWxjXfFekXvHnS7fmE30Wg";
+
+        List<Map<String, String>> result = login.verifyToken(jwtToken);
+
+        List<Map<String, String>> expected = new ArrayList<>();
+        expected.add(1, Map.of("GRADING", "READ"));
+        expected.add(2, Map.of("GRADING", "WRITE"));
+        expected.add(3, Map.of("COURSE_FEEDBACK", "READ"));
+
+        assertIterableEquals(expected, result);
+    }
+
+    @Test
+    public void verify_token_test_two() {
+        when(userRepository.findAll()).thenReturn(List.of(
+                        new AppUser(3, "kalle", "password", "STUDENT")
+                )
+        );
+        String jwtToken = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJrYWxsZSIsIlJvbGUiOiJTVFVERU5UIn0.wubfKhTSs_uSPIqkM1xkuuSl0J4zr1nj3U8fMMyP1VKOADXjBVjVqfR5oz1rEJrJ";
+
+        List<Map<String, String>> result = login.verifyToken(jwtToken);
+
+        List<Map<String, String>> expected = new ArrayList<>();
+        expected.add(1, Map.of("GRADING", "READ"));
+        expected.add(3, Map.of("COURSE_FEEDBACK", "READ"));
+        expected.add(3, Map.of("COURSE_FEEDBACK", "WRITE"));
 
         assertIterableEquals(expected, result);
     }
